3056a1c4619c85dd951a8fed9da0580bdfb03fd2
Raghav Jindal <40332753+RaghavJindal2000@users.noreply.github.com>
2021-12-09 14:19:55 +0530

    Commit
    Commit

---
+++ Key-Logs-main/anti-keylogger.py
@@ -0,0 +1,13 @@
+import os
+import time
+import psutil
+
+# "someProgram" in (p.name() for p in psutil.process_iter())
+for p in psutil.process_iter():
+    print(p.name)
+print("All Existing KeyLogs Terminated!")
+
+# var = os.system("TASKKILL /F /IM keylogger.exe")
+
+# import ctypes # An included library with Python install.
+# ctypes.windll.user32.MessageBoxW(0, "Keylogger has been terminated", "Keylogger", 1)
